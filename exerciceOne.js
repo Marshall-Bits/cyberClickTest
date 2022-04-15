f5455b5524d1074eb0559c4f4c9d228623e536e1
Marshall-Bits <kz_@hotmail.es>
2022-04-15 09:58:24 +0200

    added html

--- exerciceOne.js
+++ exerciceOne.js
@@ -3,25 +3,31 @@
 // por la cadena “cyber”, los múltiplos de 5 por la cadena “click” y los que son múltiplos
 // de 3 y de 5 por “cyberclick”.
 
+console.log("script working")
+const multipleButton = document.querySelector("#multiples");
+const multipleList = document.querySelector(".multiple-list")
 
-function findMultiples(){
-    for(let i=0; i<=100; i++){
-        switch (i) {
-            case i%3===0 && i%5===0:
-                console.log("cyberclick");
-                break;
-            case i%3===0:
-                console.log("cyber");    
-                break;
-            case i%5===0:
-                console.log("click");
-                break;
-            default:
-                console.log(i);
-                break;
-        }
-    }
+function addToList(element){
+    multipleList.appendChild(document.createElement("li")).innerText=element;
 }
 
+function findMultiples(){
+    for(let i=1; i<=100; i++){
+        if(i%3===0 && i%5===0){
+            addToList("cyberclick");
+        }
+        else if(i%3===0){
+            addToList("cyber");
+        }
+        else if(i%5===0){
+            addToList("cyber");
+        }
+        else {
+            addToList(i)
+        }
+    }
 
+}
+
+multipleButton.addEventListener("click", findMultiples)
 
